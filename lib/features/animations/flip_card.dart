1cbae1aadabae88d43c5d2e3d9980e8ad6a6f868
mmh-masud03 <2054901003@student.bup.edu.bd>
2024-12-30 00:15:15 +0600

    flip card added

---
+++ lib/features/animations/flip_card.dart
@@ -0,0 +1,80 @@
+// Flip Card Animation
+import 'package:flutter/material.dart';
+
+class FlipCard extends StatefulWidget {
+  final Widget front;
+  final Widget back;
+  final Duration duration;
+
+  const FlipCard({
+    Key? key,
+    required this.front,
+    required this.back,
+    this.duration = const Duration(milliseconds: 800),
+  }) : super(key: key);
+
+  @override
+  State<FlipCard> createState() => _FlipCardState();
+}
+
+class _FlipCardState extends State<FlipCard> with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+  bool _isFrontVisible = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: widget.duration,
+      vsync: this,
+    );
+    _animation = TweenSequence<double>([
+      TweenSequenceItem(
+        tween: Tween<double>(begin: 0.0, end: pi / 2),
+        weight: 50,
+      ),
+      TweenSequenceItem(
+        tween: Tween<double>(begin: -pi / 2, end: 0.0),
+        weight: 50,
+      ),
+    ]).animate(_controller);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  void _flip() {
+    if (_controller.isAnimating) return;
+    if (_isFrontVisible) {
+      _controller.forward();
+    } else {
+      _controller.reverse();
+    }
+    _isFrontVisible = !_isFrontVisible;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: _flip,
+      child: AnimatedBuilder(
+        animation: _animation,
+        builder: (context, child) {
+          final angle = _animation.value;
+          final transform = Matrix4.identity()
+            ..setEntry(3, 2, 0.001)
+            ..rotateY(angle);
+          return Transform(
+            transform: transform,
+            alignment: Alignment.center,
+            child: angle < pi / 2 ? widget.front : widget.back,
+          );
+        },
+      ),
+    );
+  }
+}
