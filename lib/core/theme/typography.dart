df44268d784113d1fe594b3c0a8a87743f38dfe0
mmh-masud03 <2054901003@student.bup.edu.bd>
2025-01-10 23:53:37 +0600

    typography file added

--- lib/core/theme/typography.dart
+++ lib/core/theme/typography.dart
@@ -1,1 +1,80 @@
- 
+//add some awesome typography here
+import 'package:flutter/material.dart';
+
+class Typography {
+  static const TextStyle headline1 = TextStyle(
+    fontSize: 96,
+    fontWeight: FontWeight.w300,
+    letterSpacing: -1.5,
+  );
+
+  static const TextStyle headline2 = TextStyle(
+    fontSize: 60,
+    fontWeight: FontWeight.w300,
+    letterSpacing: -0.5,
+  );
+
+  static const TextStyle headline3 = TextStyle(
+    fontSize: 48,
+    fontWeight: FontWeight.w400,
+  );
+
+  static const TextStyle headline4 = TextStyle(
+    fontSize: 34,
+    fontWeight: FontWeight.w400,
+    letterSpacing: 0.25,
+  );
+
+  static const TextStyle headline5 = TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.w400,
+  );
+
+  static const TextStyle headline6 = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.w500,
+    letterSpacing: 0.15,
+  );
+
+  static const TextStyle subtitle1 = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w400,
+    letterSpacing: 0.15,
+  );
+
+  static const TextStyle subtitle2 = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w500,
+    letterSpacing: 0.1,
+  );
+
+  static const TextStyle bodyText1 = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w400,
+    letterSpacing: 0.5,
+  );
+
+  static const TextStyle bodyText2 = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+    letterSpacing: 0.25,
+  );
+
+  static const TextStyle button = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w500,
+    letterSpacing: 1.25,
+  );
+
+  static const TextStyle caption = TextStyle(
+    fontSize: 12,
+    fontWeight: FontWeight.w400,
+    letterSpacing: 0.4,
+  );
+
+  static const TextStyle overline = TextStyle(
+    fontSize: 10,
+    fontWeight: FontWeight.w400,
+    letterSpacing: 1.5,
+  );
+}
